cb7342f9e7557946a87c6a48ef20b60ffe95db69
Paola Pappalardo <162357210+paolaapap@users.noreply.github.com>
2024-05-15 22:24:48 +0200

    Add files via upload

--- reset_password.php
+++ reset_password.php
@@ -1,90 +1,80 @@
 <?php
 require_once 'auth.php';
- 
-if(isset($_SESSION['token']))
-{
-    //se il token inserito è uguale a session token AND esiste quel token in reset_passowrd allora
-    //-> se in reset_password quel token è associato a quella email -> fai la query che cambia la password, fai la sessione con session email e chiudi la sessione con session token e manda in index_logged
-
-    //se il token è sbagliato ERRORE con stringa rossa in alto "This token is not valid. Try again." (cioè se il token non esiste nel db)
-    //se la query non restituisce risultati ERRORE con stringa rossa in alto "There is not a token associated with this email. Return to forgot password section" (cioe se nel db non esiste la riga email & token)
+if(isset($_SESSION['token'])){
     if(isset($_POST['token']) && isset($_POST['email']) && isset($_POST['password']) && isset($_POST['password_confirm'])){
-        $conn = mysqli_connect($dbconfig['host'], $dbconfig['user'], $dbconfig['password'], $dbconfig['database']) or die("Errore: " .mysqli_connect_error());
 
+        $error = array();
+        $conn = mysqli_connect($dbconfig['host'], $dbconfig['user'], $dbconfig['password'], $dbconfig['database']) or die("Errore: " .mysqli_connect_error());    
         
+        #CONTROLLO LA PASSWORD
+        if(!(preg_match('/.{8,}/', $_POST['password']) && preg_match('/[A-Z]/', $_POST['password']) && preg_match('/[a-z]/', $_POST['password']) &&
+            preg_match('/[0-9]/', $_POST['password']) && preg_match('/[!@#$%^&*(),.?]/', $_POST['password']))){
+            $error[] = "Password not valid.";
+        }
+
+        #CONTROLLO MATCH TRA LE PASSWORD
+        if (strcmp($_POST['password'], $_POST['password_confirm']) != 0) {
+            $error[] = "Passowrds don't match.";
+        }
+
+        #CONTROLLO ASSOCIAZIONE TOKEN-SESSIONE
+        if($_POST['token'] != $_SESSION['token']){
+            $error[] = "Tokens don't match.";
+        }
+
+        #CONTROLLO CHE IL TOKEN ESISTA NEL DB
         $token = mysqli_real_escape_string($conn, $_POST['token']);
-        $email = mysqli_real_escape_string($conn, strtolower($_POST['email']));
-        $password = mysqli_real_escape_string($conn, $_POST['password']);
-        $password = password_hash($password, PASSWORD_BCRYPT);
-        $password_confirm = mysqli_real_escape_string($conn, $_POST['password_confirm']);
-        $password_confirm = password_hash($password_confirm, PASSWORD_BCRYPT);
-        
-
         $query_token = "SELECT * FROM reset_password WHERE token='$token'";
         $res = mysqli_query($conn, $query_token)  or die("Errore: ". mysqli_connect_error());
-
-        if($token == $_SESSION["token"] && mysqli_num_rows($res) > 0)
-        { //verifico se la sessione è associata al token giusto e se il token effettivamente esiste nel db
-          
-            $query_control = "SELECT * FROM reset_password WHERE email ='" . $email . "' AND token = '" .$token."'";
-            $res2 = mysqli_query($conn, $query_control)  or die("Errore: ". mysqli_connect_error());
-
-            if(mysqli_num_rows($res2) > 0 && (strcmp($_POST['password'], $_POST['password_confirm']) == 0))
-            { //allora permette la modifica della password, fa la sessione con session email ed elimina la sessione del token e manda ad index_logged
-                  if(!(preg_match('/.{8,}/', $_POST['password']) && preg_match('/[A-Z]/', $_POST['password']) && preg_match('/[a-z]/', $_POST['password']) &&
-                      preg_match('/[0-9]/', $_POST['password']) && preg_match('/[!@#$%^&*(),.?]/', $_POST['password']))){
-                      $error_password = true;
-                  } else {
-                    $error_password = false;
-                  }
-                  
-                  if($error_password == "false"){
-                      $query_update = "UPDATE users SET password ='" . $password . "' WHERE email ='" . $email . "'";
-                      $res3 = mysqli_query($conn, $query_update)  or die("Errore: ". mysqli_connect_error());
-                      
-                      if($res3)
-                      {
-                        $query_id ="SELECT * FROM users WHERE email = '$email'";
-                        $res4 = mysqli_query($conn, $query_id) or die("Errore: ". mysqli_connect_error());
-                        if(mysqli_num_rows($res4) > 0){
-                          $userinfo = mysqli_fetch_assoc($res4);
-                          $userid = $userinfo["id"];
-                        } else {
-                          $error_found = true;
-                        }
-                        unset($_SESSION['token']);
-                        $_SESSION["user_id"] = $userid;
-                        header("Location: index_logged.php"); 
-                        mysqli_free_result($res3);
-                        mysqli_free_result($res4);
-                        exit;  
-                      } 
-                      else 
-                      {
-                        $error = true;
-                      }
-                  }
-            }
-            else
-            {
-              $not_association = true;
-            }
-
-        } 
-        else
-        {
-          $token_notvalid=true;
+        if(mysqli_num_rows($res) == 0){
+            $error[] = "Token doesn't exists.";
         }
-        mysqli_free_result($res);
-        mysqli_free_result($res2); //sistema questo codice altrimenti dopo exit queste non verranno mai cagate
+
+        #CONTROLLO CHE ESISTA LA RIGA EMAIL-TOKEN
+        $email = mysqli_real_escape_string($conn, strtolower($_POST['email']));
+        $query_check = "SELECT * FROM reset_password WHERE email ='" . $email . "' AND token = '" .$token."'"; 
+        $res = mysqli_query($conn, $query_check)  or die("Errore: ". mysqli_connect_error());
+        if(mysqli_num_rows($res) == 0){
+            $error[] = "Wrong token.";
+        }
+
+        #SE NON CI SONO STATI ERRORI, FACCIO L'UPDATE
+        if(count($error) == 0){
+            
+            $email = mysqli_real_escape_string($conn, strtolower($_POST['email']));
+            $password = mysqli_real_escape_string($conn, $_POST['password']);
+            $password = password_hash($password, PASSWORD_BCRYPT);
+
+            $query_update = "UPDATE users SET password ='" . $password . "' WHERE email ='" . $email . "'";
+            $res = mysqli_query($conn, $query_update)  or die("Errore: ". mysqli_connect_error());
+
+            //se l'update è andato bene cambio la sessione
+            if($res){ 
+                $query_id ="SELECT * FROM users WHERE email = '" . $email . "'";
+                $res = mysqli_query($conn, $query_id) or die("Errore: ". mysqli_connect_error());
+                
+                if(mysqli_num_rows($res) > 0){
+                    $userinfo = mysqli_fetch_assoc($res);
+                    $userid = $userinfo["id"];
+                    unset($_SESSION['token']);
+                    $_SESSION["user_id"] = $userid;
+                    header("Location: index_logged.php"); 
+                    exit;  
+                }
+                else{
+                    $error[] = "Problem on query_id.";
+                }
+            } else {
+                $error[]= "Problem during update.";
+            }
+        }
 
         mysqli_close($conn);
     }
-} 
-else
-{
+
+} else {
     header("Location: forgot_password.php");
-    exit;
+    exit;    
 }
 ?>
 
@@ -106,30 +96,12 @@ else
           <div id="reset_password">
             <img id="logo" src="images\logo.png"/>
             <h1>Reset your password.</h1>
-            <?php
-                if(isset($token_notvalid)){
-                    echo "<h1 class='error_php'>";
-                    echo "This token is not valid. Try again. ";
-                    echo "</h1>";
-                } 
-
-                if(isset($not_association)){
-                  echo "<h1 class='error_php'>";
-                  echo "There is not a token associated with this email. Return to forgot password section.";
-                  echo "</h1>";
-                } 
-                if(isset($error)){
-                  echo "<h1 class='error_php'>";
-                  echo "Unexpected error while changing password. Please try again.";
-                  echo "</h1>";
-              } 
-
-
-                if(isset($error_found)){
-                  echo "<h1 class='error_php'>";
-                  echo "Lalalal";
-                  echo "</h1>";
-              } 
+            <?php 
+            if(isset($error)) {
+                    foreach($error as $err) {
+                        echo "<div class='error_php'>".$err."</div>";
+                    }
+                  } 
             ?>
             <form name="form_reset_password" method="post">
 
